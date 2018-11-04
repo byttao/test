ce2eb3483cdf7f44a46198c90b46c150c6491914
白鷐 半妖 <byttao@gmail.com>
2018-11-04 20:13:26 +0800

    搞定

--- testform/Form1.Designer.cs
+++ testform/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(644, 193);
+            this.button1.Location = new System.Drawing.Point(24, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 38);
             this.button1.TabIndex = 0;
@@ -44,21 +49,43 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(20, 14);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(589, 378);
+            this.richTextBox1.Size = new System.Drawing.Size(696, 385);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(696, 450);
+            this.splitContainer1.SplitterDistance = 385;
+            this.splitContainer1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(696, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,6 +94,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- testform/Form1.cs
+++ testform/Form1.cs
@@ -24,13 +24,15 @@ namespace testform
             List<Data> datas = new List<Data>();//定义1个列表用于保存结果
             HtmlWeb hw = new HtmlWeb();
             hw.OverrideEncoding = Encoding.GetEncoding("UTF-8");
-            HtmlDocument htmlDocument = hw.Load(@"D:\lenovo\Desktop\税金.html");
+            HtmlDocument htmlDocument = hw.Load(@"D:\ProJect\税金.html");
             HtmlNodeCollection collection = htmlDocument.DocumentNode.SelectSingleNode("//table[@id='frm_dfsf']").ChildNodes;//跟Xpath一样，轻松的定位到相应节点下
             foreach (HtmlNode node in collection)
             {
                 
                 //去除\r\n以及空格，获取到相应td里面的数据13
-                string[] line = node.InnerText.Split(new char[] { '\r', '\n','\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] line = node.InnerText.Replace("\r","").Replace("\n","").
+                    Replace("\t","").Replace(" ","").Replace("&nbsp;","\r").
+                    Split(new char[] {'\r' }, StringSplitOptions.RemoveEmptyEntries);
 
                 //如果符合条件，就加载到对象列表里面
                 if (line.Length == 18)
@@ -53,7 +55,30 @@ namespace testform
                         扣款返回信息 = line[14],
                         缴款方式 = line[15],
                         银行帐号 = line[16],
-                        入库日期 = line[17],
+                        入库日期 = line[17]
+                    });
+
+                if (line.Length == 15)
+                    datas.Add(new Data()
+                    {
+                        申报方式 = line[0],
+                        税票号 = line[1],
+                        征收项目 = line[2],
+                        征收品目 = line[3],
+                        申报日期 = line[4],
+                        税款所属期始 = line[5],
+                        税款所属期止 = line[6],
+                        计税依据 = line[7],
+                        税率 = line[8],
+                        应纳税额 = line[9],
+                        已缴税额 = line[10],
+                        实际应缴税额 = line[11],
+                        缴款日期 = line[12],
+                        处理状态 = line[13],
+                        扣款返回信息 = line[14],
+                        缴款方式 = "",
+                        银行帐号 = "",
+                        入库日期 = ""
                     });
             }
 
